a811dc9f8062a6d4981285fa521ae22d80a3cb67
gman.park <im@gman.io>
2017-01-22 12:44:43 +0900

    [TASK] init

---
+++ configs/webpack.config.js
@@ -0,0 +1,62 @@
+/**
+ * Created on 2017-01-21.
+ * @author: Gman Park
+ */
+
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    context: path.resolve(process.cwd(), './src'),
+    entry: {
+        app: './app.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ["es2015"]
+                        ]
+                    }
+                }],
+            },
+        ],
+    },
+    output: {
+        path: path.resolve(process.cwd(), './dist'),
+        filename: '[name].bundle.js',
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            minimize: true,
+            debug: false
+        }),
+    ],
+
+    devServer: {
+        contentBase: path.resolve(process.cwd(), './'),  // New
+        index: "src/index.html",
+        // the index path for web server
+        stats: {
+            colors: true
+        },
+        // options for formating the statistics
+        serverSideRender: false,
+        // Turn off the server-side rendering mode. See Server-Side Rendering part for more info.
+        noInfo: false,
+        // display no info to console (only warnings and errors)
+        quiet: true,
+        // display nothing to the console
+        lazy: true,
+        // switch into lazy mode
+        // that means no watching, but recompilation on every request
+        watchOptions: {
+            aggregateTimeout: 300,
+            poll: true
+        },
+    },
+};
---
+++ src/app.js
@@ -0,0 +1,92 @@
+/**
+ * Created on 2017-01-21.
+ * @author: Gman Park
+ */
+import Rx from 'rxjs/Rx';
+
+const AppKey = 'f0c12da8-2ef4-3e24-8815-e2f347909a80';
+let searchKeyword = "nike";
+
+class App {
+    constructor() {
+        this.attachEvent();
+
+        //Init load datas.
+        this.requestData();
+    }
+
+    attachEvent() {
+        let input = document.querySelector('.inp');
+
+        Rx.Observable.fromEvent(input, 'keyup')
+            // .debounceTime(1000)
+            .map(function (e) {
+                return e.target.value;
+            })
+            .distinctUntilChanged()
+            // .scan(function (prev, current) {
+            //     if (prev == null) {
+            //         return null;
+            //     }
+            //     return current;
+            // }, null)
+            .filter(function (text) {
+                // searchText null validation.
+                console.log(text);
+                return text != null
+            })
+            .subscribe(
+                text => {
+                    this.searchKeyword = text;
+                    if (text != null) {
+                        this.requestData();
+                    }
+                },
+                (err) => {
+                    console.log(err);
+                }
+            );
+    }
+
+    requestData() {
+        Rx.Observable
+            .ajax({
+                url: 'http://apis.skplanetx.com/11st/v2/common/products?appKey=' + AppKey + '&searchKeyword=' + this.searchKeyword + '&sortCode=A',
+                crossDomain: true,
+                headers: {
+                    "Content-type": "application/json",
+                    "Accept": "application/json"
+                }
+            })
+            .retry(3)
+            .map(e => e.response)
+            .map(e => {
+                if (e.ProductSearchResponse) {
+                    return e.ProductSearchResponse.Products.Product
+                }
+            })
+            .subscribe(
+                (res) => {
+                    let wrapper = document.querySelector('.item_list');
+
+                    wrapper.innerHTML = "";
+
+                    for (var i = 0; i < res.length; i++) {
+
+                        // Html5 <template> import
+                        let template = document.querySelector('#tmpl_item');
+                        let elImg = template.content.querySelector('img');
+                        elImg.src = res[i].ProductImage;
+
+                        let clone = document.importNode(template.content, true);
+                        wrapper.appendChild(clone);
+                    }
+                },
+                (err) => {
+                    console.log(err);
+                }
+            );
+    }
+}
+
+new App();
